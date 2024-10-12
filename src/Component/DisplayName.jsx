21f0c45ac316f88f50160264ae548ad8ed706558
iamanjali <iamanjalijaiswal@gmail.com>
2024-10-12 16:57:59 +0530

    Display Name updated

--- src/Component/DisplayName.jsx
+++ src/Component/DisplayName.jsx
@@ -1,33 +1,35 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
-function DisplayName () {
-   let [fullName, setFullName] = useState("");
-   let [flag, setFlag] = useState(false);
+function DisplayName() {
+  let [fullName, setFullName] = useState("");
 
-    let handleSubmit = (event) => {
-        event.preventDefault();
+  let handleSubmit = (event) => {
+    event.preventDefault();
 
-        let fname = event.target.elements.fname.value;
-        let lname = event.target.elements.lname.value;
-
-        let inputValue = `${fname} ${lname}`;
-        setFullName(inputValue);
-        setFlag(!flag);
+    let fname = event.target.elements.fname.value.trim();
+    let lname = event.target.elements.lname.value.trim();
+    if (fname && lname) {
+      setFullName(`${fname} ${lname}`);
+    } else {
+      setFullName("");
     }
+  };
 
-    // useEffect = (() => {
-    //     setFlag(!flag);
-    // },[flag])
 
-    return <>
-    <form onSubmit={handleSubmit}>
-        First Name:<input type="text" name="fname" required/>
-        Last Name:<input type="text" name="lname" required/>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        First Name:
+        <input type="text" name="fname" required />
+        Last Name:
+        <input type="text" name="lname" required />
         <button type="submit">Submit</button>
-    </form>
+      </form>
 
-    <p>{flag ? `Full Name: ${fullName}` : ""}</p>
+      {/* <p>{flag ? `Full Name: ${fullName}` : ""}</p> */}
+      {fullName && <p>Full Name: {fullName}</p>}
     </>
+  );
 }
 
 export default DisplayName;
